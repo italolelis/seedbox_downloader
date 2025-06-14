1444104ac189e724011e9c5145c009aac0cde984
italolelis <me@italovietro.com>
2025-06-14 22:27:56 +0000

    Refactor main.go to enhance download client setup and server initialization
    - Introduced a factory method to build the download client based on configuration, supporting both Deluge and Put.io clients.
    - Added a setupServer function to prepare the HTTP server with appropriate routes and handlers for the Put.io client.
    - Implemented a cleanup setup function to manage expired downloads, improving resource management.
    - Enhanced the main run function to include error handling for server operations and structured initialization of components.

--- cmd/seedbox_downloader/main.go
+++ cmd/seedbox_downloader/main.go
@@ -4,13 +4,19 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/italolelis/seedbox_downloader/internal/cleanup"
 	"github.com/italolelis/seedbox_downloader/internal/config"
+	"github.com/italolelis/seedbox_downloader/internal/dc"
 	"github.com/italolelis/seedbox_downloader/internal/dc/deluge"
+	"github.com/italolelis/seedbox_downloader/internal/dc/putio"
 	"github.com/italolelis/seedbox_downloader/internal/downloader"
+	"github.com/italolelis/seedbox_downloader/internal/http/rest"
 	"github.com/italolelis/seedbox_downloader/internal/logctx"
 	"github.com/italolelis/seedbox_downloader/internal/notifier"
 	"github.com/italolelis/seedbox_downloader/internal/storage/sqlite"
@@ -40,6 +46,8 @@ func main() {
 func run(ctx context.Context, cfg *config.Config) error {
 	logger := logctx.LoggerFromContext(ctx)
 
+	// =========================================================================
+	// Start Database
 	database, err := sqlite.InitDB(cfg.DBPath)
 	if err != nil {
 		logger.Error("DB error", "err", err)
@@ -50,20 +58,47 @@ func run(ctx context.Context, cfg *config.Config) error {
 
 	dr := sqlite.NewDownloadRepository(database)
 
-	dlClient := deluge.NewClient(cfg.DelugeBaseURL, cfg.DelugeAPIURLPath, cfg.DelugeCompletedDir, cfg.DelugeUsername, cfg.DelugePassword, true)
-	if err := dlClient.Authenticate(ctx); err != nil {
-		return fmt.Errorf("Deluge authentication error: %w", err)
+	// =========================================================================
+	// Start Download Client
+	dc, err := buildDownloadClient(cfg)
+	if err != nil {
+		return fmt.Errorf("failed to build download client: %w", err)
 	}
 
+	if err := dc.Authenticate(ctx); err != nil {
+		return fmt.Errorf("authentication error: %w", err)
+	}
+
+	// =========================================================================
+	// Start Downloader
 	downloader := downloader.NewDownloader(
 		dr,
 		cfg.TargetDir,
 		cfg.TargetLabel,
-		dlClient,
+		dc,
 		cfg.MaxParallel,
 	)
 
+	// =========================================================================
+	// Start Notification
 	setupNotificationForDownloader(ctx, downloader, cfg)
+
+	// =========================================================================
+	// Start API Service
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	server, err := setupServer(ctx, dc, cfg)
+	if err != nil {
+		return fmt.Errorf("failed to setup server: %w", err)
+	}
+
+	go func() {
+		logger.Info("Initializing API support", "host", cfg.Web.BindAddress)
+		serverErrors <- server.ListenAndServe()
+	}()
 
 	logger.Info("waiting for downloads...",
 		"target_label", cfg.TargetLabel,
@@ -72,39 +107,33 @@ func run(ctx context.Context, cfg *config.Config) error {
 		"retention", cfg.KeepDownloadedFor.String(),
 	)
 
-	// Start independent cleanup goroutine
-	cleanupTicker := time.NewTicker(cfg.CleanupInterval)
-	defer cleanupTicker.Stop()
+	// =========================================================================
+	// Start Cleanup
+	setupCleanup(ctx, dr, cfg)
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				logger.Info("cleanup goroutine shutting down.")
-
-				return
-			case <-cleanupTicker.C:
-				tracked, err := dr.GetDownloads()
-				if err != nil {
-					logger.Error("failed to get tracked downloads for cleanup", "err", err)
-
-					continue
-				}
-
-				if err := cleanup.DeleteExpiredFiles(ctx, tracked, cfg.TargetDir, cfg.KeepDownloadedFor); err != nil {
-					logger.Error("failed to delete expired tracked files", "err", err)
-				}
-			}
-		}
-	}()
-
+	// =========================================================================
+	// Start Main Loop
 	ticker := time.NewTicker(cfg.UpdateInterval)
 	defer ticker.Stop()
 
 	for {
 		select {
+		case err := <-serverErrors:
+			return fmt.Errorf("server error: %w", err)
 		case <-ctx.Done():
-			logger.Info("context cancelled, shutting down main loop.")
+			logger.Info("start shutdown")
+
+			// Give outstanding requests a deadline for completion.
+			ctx, cancel := context.WithTimeout(ctx, cfg.Web.ShutdownTimeout)
+			defer cancel()
+
+			if err := server.Shutdown(ctx); err != nil {
+				logger.Error("failed to gracefully shutdown the server", "err", err)
+
+				if err = server.Close(); err != nil {
+					return fmt.Errorf("could not stop server gracefully: %w", err)
+				}
+			}
 
 			return ctx.Err()
 		case <-ticker.C:
@@ -143,6 +172,72 @@ func setupNotificationForDownloader(ctx context.Context, downloader *downloader.
 				"✅ Download finished for torrent: " + event.Name + " (" + event.ID + ")",
 			); notifyErr != nil {
 				logger.Error("failed to send notification", "download_id", event.ID, "err", notifyErr)
+			}
+		}
+	}()
+}
+
+// This is an abstract factory for the download client.
+func buildDownloadClient(cfg *config.Config) (dc.DownloadClient, error) {
+	switch cfg.DownloadClient {
+	case "deluge":
+		return deluge.NewClient(cfg.DelugeBaseURL, cfg.DelugeAPIURLPath, cfg.DelugeCompletedDir, cfg.DelugeUsername, cfg.DelugePassword, true), nil
+	case "putio":
+		return putio.NewClient(cfg.PutioBaseURL, cfg.PutioToken, true), nil
+	}
+
+	return nil, fmt.Errorf("invalid download client: %s", cfg.DownloadClient)
+}
+
+// setupServer prepares the handlers and services to create the http rest server.
+func setupServer(ctx context.Context, dc dc.DownloadClient, cfg *config.Config) (*http.Server, error) {
+	var tHandler *rest.TransmissionHandler
+
+	if dc, ok := dc.(*putio.Client); ok {
+		tHandler = rest.NewTransmissionHandler(cfg.Transmission.Username, cfg.Transmission.Password, dc, cfg.TargetLabel, cfg.TargetDir)
+	} else {
+		return nil, fmt.Errorf("download client is not a putio client: %s", cfg.DownloadClient)
+	}
+
+	r := chi.NewRouter()
+	r.Mount("/", tHandler.Routes())
+
+	return &http.Server{
+		Addr:         cfg.Web.BindAddress,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		Handler:      r,
+		BaseContext: func(net.Listener) context.Context {
+			return ctx
+		},
+	}, nil
+}
+
+func setupCleanup(ctx context.Context, dr *sqlite.DownloadRepository, cfg *config.Config) {
+	logger := logctx.LoggerFromContext(ctx)
+
+	cleanupTicker := time.NewTicker(cfg.CleanupInterval)
+	defer cleanupTicker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				logger.Info("cleanup goroutine shutting down.")
+
+				return
+			case <-cleanupTicker.C:
+				tracked, err := dr.GetDownloads()
+				if err != nil {
+					logger.Error("failed to get tracked downloads for cleanup", "err", err)
+
+					continue
+				}
+
+				if err := cleanup.DeleteExpiredFiles(ctx, tracked, cfg.TargetDir, cfg.KeepDownloadedFor); err != nil {
+					logger.Error("failed to delete expired tracked files", "err", err)
+				}
 			}
 		}
 	}()
