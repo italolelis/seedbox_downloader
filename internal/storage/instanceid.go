493a799179c55b317119ddd4140c5ef6acfd2f31
italolelis <me@italovietro.com>
2025-06-16 14:56:06 +0000

    Add GenerateInstanceID function to create unique process identifiers
    - Introduced a new function in instanceid.go that generates a unique string based on the hostname, process ID, and a random byte sequence.
    - This function enhances the ability to uniquely identify instances of the application, which can be useful for logging and monitoring purposes.

---
+++ internal/storage/instanceid.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+	"strconv"
+)
+
+// GenerateInstanceID returns a unique string for this process (hostname+pid+random).
+func GenerateInstanceID() string {
+	host, _ := os.Hostname()
+	pid := os.Getpid()
+	rnd := make([]byte, 4)
+	_, _ = rand.Read(rnd)
+
+	return host + "-" + strconv.Itoa(pid) + "-" + hex.EncodeToString(rnd)
+}
